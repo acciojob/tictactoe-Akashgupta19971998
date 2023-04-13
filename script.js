b2af10bf241a3bd311bd6e133b5a6e20b17b683a
Akashgupta19971998 <akashguptaakash2@gmail.com>
2023-04-13 15:19:14 +0530

    commit

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let player1=document.getElementById("player1");
+let player2=document.getElementById("player2");
+let toBeHidden=document.getElementsByClassName("container");
+let message=document.getElementsByClassName("message");
+function click(){
+toBeHidden.style.display="none";
+message.innerHTML=player1.value +" , you're up";
+}
